3e31cae5781b44ca1ef5e971a332bbe193657b0b
doriera <adrian.dorier@students.ffhs.ch>
2020-06-24 13:44:06 +0200

    working

--- app/src/main/java/com/example/lightup/MainActivity.java
+++ app/src/main/java/com/example/lightup/MainActivity.java
@@ -31,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
      */
     protected Session session;
 
+
+    /**
+     * The MainActivity is created by this method (generated from a XML)
+     *
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
         //To set the old selected color
         picker.setOldCenterColor(picker.getColor());
-
 
 
         // onColorChanged (which detects every slight deviation as a different number) calls the method to setUp a SSH connection and exec the handed command
@@ -85,19 +90,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public ArrayList<Integer> aarrggbbConverter(int argb) {
-        int r = (argb>>16)&0xFF;
-        int g = (argb>>8)&0xFF;
-        int b = (argb)&0xFF;
 
-        ArrayList<Integer> rgb = new ArrayList<>();
-
-        rgb.add(r);
-        rgb.add(g);
-        rgb.add(b);
-
-        return rgb;
-    }
 
 
     /**
@@ -105,20 +98,39 @@ public class MainActivity extends AppCompatActivity {
      * @param rgb ArrayList with converted RGB
      */
     @SuppressLint("StaticFieldLeak")
-    public void setUpCommand(ArrayList<Integer> rgb) {
+    public void setUpCommand(final ArrayList<Integer> rgb) {
 
         for (int i = 0; i <3; i++) {
             Log.v("color", rgb.get(i).toString());
         }
 
+        // testscript command to test circuit
+        //final String command = "sudo python lightUp.py";
 
-        final String command = "sudo python lightUp.py";
+        /*
+        // command for the pin 17 RED
+        String command1 = "pigs p 17 " + rgb.get(0).toString();
+        // command for the pin 22 GREEN
+        String command2 = "pigs p 22 " + rgb.get(1).toString();
+        // command for the pin 24 BLUE
+        String command3 = "pigs p 24 " + rgb.get(2).toString();
+
+        ArrayList<String> commands = new ArrayList<>();
+
+        commands.add(command1);
+        commands.add(command2);
+        commands.add(command3);
+         */
+
+
+
+        // to do this simultaneously apart from this main_activity thread, a new one is created
         new AsyncTask<Integer, Void, Void>(){
             @SuppressLint("StaticFieldLeak")
             @Override
             protected Void doInBackground(Integer... params) {
                 try {
-                    executeSSHcommand(command);
+                    executeSSHcommand(rgb);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -127,35 +139,68 @@ public class MainActivity extends AppCompatActivity {
         }.execute(1);
     }
 
-
-
-
     /**
      * Once the session has connected, a new channel gets opened. (exec is a command for the bash, that sets the session to an executable command)
      * it is the channel that gets the effective command
      * set a session to the Pi so every executeSSHcommand can be executed by a connected session with a new JSch instance
      * the login is used by the user default user pi
      */
-    public void executeSSHcommand(String command) {
+    public void executeSSHcommand(ArrayList<Integer> rgb) {
         String user = "pi";
         String password = "raspberry";
-        String host = "192.168.3.42";
+        String host = "192.168.10.99";
+
+        // command for the pin 17 RED
+        String color1 = "pigs p 17 " + rgb.get(0);
+        // command for the pin 22 GREEN
+        String color2 = "pigs p 22 " + rgb.get(1);
+        // command for the pin 24 BLUE
+        String color3 = "pigs p 24 " + rgb.get(2);
+
+        ArrayList<String> commands = new ArrayList<>();
+        commands.add(color1);
+        commands.add(color2);
+        commands.add(color3);
+
         int port = 22;
         try {
             JSch jsch = new JSch();
             session = jsch.getSession(user, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
-            session.setTimeout(100000);
+            session.setTimeout(10000);
             session.connect();
-            ChannelExec channel = (ChannelExec) session.openChannel("exec");
-            channel.setCommand(command);
-            System.out.println(command);
-            channel.connect();
-            channel.disconnect();
+            for (int i = 0; i < 3; i++) {
+                ChannelExec channel = (ChannelExec) session.openChannel("exec");
+                channel.setCommand(commands.get(i));
+                channel.connect();
+                channel.disconnect();
+            }
         }
         catch (JSchException e) {
             e.printStackTrace();
         }
+    }
+
+
+
+    /**
+     * This is a converter method
+     * @param argb negative int value from the Holo Color Picker
+     * @return an ArrayList<Integer> with 3 numbers from 0 to 255, the RGB colors
+     */
+    public ArrayList<Integer> aarrggbbConverter(int argb) {
+        int r = (argb>>16)&0xFF;
+        int g = (argb>>8)&0xFF;
+        int b = (argb)&0xFF;
+
+        ArrayList<Integer> rgb = new ArrayList<>();
+
+        // adding to ArrayList
+        rgb.add(r);
+        rgb.add(g);
+        rgb.add(b);
+
+        return rgb;
     }
 }
